acbab7b435aa737ad13ce7f06cda319a29fe0e96
Melissa Mepham <m.a.mepham@gmail.com>
2017-12-13 00:06:55 +1300

    trying to think about how the JS works

---
+++ calc.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', start)
+
+function start () {
+  bindEventListeners(document.getElementsByClassName('calculator')[0].children)
+}
+
+numZero() {
+  document.getElementById("results").innerHTML = 1
+}
+
+//numOne() {}
+
+//numTwo() {}
+
+//numThree() {}
+
+//numFour() {}
+
+//numFive
+
+//numSix
+
+//numSeven
+
+//numEight
+
+//numNine
+
+//clear
+
+//subtract
+
+//add
+
+//divide
+
+//multiply
+
+//equals
